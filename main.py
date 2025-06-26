68c1ae1b4825548abb4449e442193b99c86ede90
Upama04 <ciyabasak0440@gmail.com>
2025-06-26 10:26:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
-st.title("My first streamlit app")
-st.write("Hello! Create a simple web application using streamlit")
+
+st.title("Interactive Streamlit App")
+
+name=st.text_input("Enter your name : ")
+
+if st.button("Submit"):
+  st.write(f"Hello, {name}! Welcome to Streamlit.")
